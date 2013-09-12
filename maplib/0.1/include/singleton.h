055d541d3201117917e409920f59b1e2ed11b15f
1 <1>
2013-09-12 14:42:13 +0400

    Added singleton.h

---
+++ maplib/0.1/include/singleton.h
@@ -0,0 +1,46 @@
+/*
+          singleton.h
+Created by infinitydao@gmail.com 
+            09/2013
+*/
+
+#ifndef __SINGLETON_H__
+#define __SINGLETON_H__
+
+#include <memory>
+
+//=========================================================================
+namespace maplib
+{
+
+template <class T>
+class Singleton
+{
+public:
+  static T* instance()
+  {
+    if( &*_inst == 0 )
+      return _inst.reset( new T );
+    return &*_inst;
+  }
+
+  static T* release()
+  {
+    return _inst.release();
+  }
+
+  static void reset( T *const ptr = 0 )
+  {
+    _inst.reset(ptr);
+  }
+
+private:
+  static std::auto_ptr<T> _inst;
+  Singleton();
+};
+
+}
+
+//=========================================================================
+
+#endif //__SINGLETON_H__
