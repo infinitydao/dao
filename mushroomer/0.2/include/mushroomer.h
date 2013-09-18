b6e99820e26c1a152edc6838a0b3941eb0ea4c15
1 <1>
2013-09-18 18:31:53 +0400

    1

---
+++ mushroomer/0.2/include/mushroomer.h
@@ -0,0 +1,15 @@
+/*              mushroomer.h
+      created by infinitydao@gmail.com
+                  09/2013
+          General Publing License v 2.0
+*/
+
+#ifndef __MUSHROOMER_H__
+#define __MUSHROOMER_H__
+
+#include <QApplication>
+
+//=========================================================================
+//=========================================================================
+
+#endif
---
+++ mushroomer/0.2/src/mushroomer.cpp
@@ -0,0 +1,15 @@
+/*              mushroomer.cpp
+      created by infinitydao@gmail.com
+                  09/2013
+          General Publing License v 2.0
+*/
+
+#include "mushroomer.h"
+
+//-------------------------------------------------------------------------
+int main( int argc, char argv[] )
+{
+  return 0;
+}
+
+//-------------------------------------------------------------------------
