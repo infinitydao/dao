e15fc4da60b6fee6293ad6e4d1aa54cdf9323aee
1 <1>
2013-09-18 18:03:30 +0400

    Added staticitemfactory.h implementation

--- gelib/0.2/include/staticitemfactory.h
+++ gelib/0.2/include/staticitemfactory.h
@@ -4,3 +4,35 @@
           General Publing License v 2.0
 */
 
+#ifndef __STATICITEMFACTORY_H__
+#define __STATICITEMFACTORY_H__
+
+//=========================================================================
+namespace gelib
+{
+
+class StaticItemFactory
+{
+protected:
+  unsigned int m_emptyCounter;
+  unsigned int m_blockCounter;
+  unsigned int m_homeCounter;
+  unsigned int m_moneyCounter;
+
+public:
+  StaticItemFactory();
+  virtual ~StaticItemFactory();
+
+  virtual IStaticItem* createItem( maplib::ItemType type, const QPoint& p, const QSize& size );
+
+  unsigned int emptyItems()const;
+  unsigned int blockItems()const;
+  unsigned int homeItems()const;
+  unsigned int moneyItems()const;
+};
+
+} //namespace gelib
+
+//=========================================================================
+
+#endif //__STATICITEMFACTORY_H__
--- gelib/0.2/src/staticitemfactory.cpp
+++ gelib/0.2/src/staticitemfactory.cpp
@@ -7,4 +7,72 @@
 #include "gelib.h"
 
 //-------------------------------------------------------------------------
+gelib::StaticItemFactory::StaticItemFactory()
+:m_emptyCounter(0),
+m_blockCounter(0),
+m_homeCounter(0),
+m_moneyCounter(0)
+{
+
+}
+
+//-------------------------------------------------------------------------
+gelib::StaticItemFactory::~StaticItemFactory()
+{
+
+}
+
+//-------------------------------------------------------------------------
+gelib::IStaticItem* gelib::StaticItemFactory::createItem( maplib::ItemType type, const QPoint&p, const QSize& size )
+{
+  switch( type ){
+    case maplib::Free:
+      m_emptyCounter++;
+      return new EmptyItem( p.x(), p.y(), size.width(), size.height() );
+      break;
+    case maplib::Block:
+      m_blockCounter++;
+      return new BlockItem( p.x(), p.y(), size.width(), size.height() );
+      break;
+    case maplib::Home:
+      if( !m_homeCounter ){
+        m_homeCounter++;
+        return new HomeItem( p.x(), p.y(), size.width(), size.height() );
+      }
+      break;
+    case maplib::Money:
+      m_moneyCounter++;
+      return new MoneyItem( p.x(). p.y(), size.width(), size.height() );
+      break;
+    default:
+      break;
+  }
+
+  return reinterpret_cast<IStaticItem*>(0);
+}
+
+//-------------------------------------------------------------------------
+unsigned int gelib::StaticItemFactory::emptyItems()const
+{
+  return m_emptyCounter;
+}
+
+//-------------------------------------------------------------------------
+unsigned int gelib::StaticItemFactory::blockItems()const
+{
+  return m_blockCounter;
+}
+
+//-------------------------------------------------------------------------
+unsigned int gelib::StaticItemFactory::homeItems()const
+{
+  return m_homeCounter;
+}
+
+//-------------------------------------------------------------------------
+unsigned int gelib::StaticItemFactory::moneyItems()const
+{
+  return m_moneyCounter;
+}
+
 //-------------------------------------------------------------------------
