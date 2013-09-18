76b06f59c76aa7c0a5076de577008964684ebceb
1 <1>
2013-09-18 18:50:02 +0400

    Привел все в порядок

---
+++ mushroomer/0.2/include/mainwindow.h
@@ -0,0 +1,20 @@
+/*              mainwindow.h
+      created by infinitydao@gmail.com
+                  09/2013
+          General Publing License v 2.0
+*/
+
+#ifndef __MAINWINDOW_H__
+#define __MAINWINDOW_H__
+
+#include <QMainWindow>
+
+//=========================================================================
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+};
+
+//=========================================================================
+
+#endif
