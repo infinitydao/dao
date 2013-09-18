c65119723d592e8d120469364091779d3f01c751
1 <1>
2013-09-18 13:03:49 +0400

    добавлен imapitem.h

--- gelib/0.2/include/imapitem.h
+++ gelib/0.2/include/imapitem.h
@@ -7,7 +7,43 @@
 #ifndef __IMAPITEM_H__
 #define __IMAPITEM_H__
 
+#include "regionitem.h"
+
 //=========================================================================
+namespace gelib
+{
+
+class IMapItem
+{
+protected:
+  maplib::ItemType m_type;                    //Тип ячейки
+  unsigned int m_health;                      //Здорвье (для персонажей и разрушаемых блоков)
+  maplib::DirectionType m_directionType;      //Направление (для движущихся объектов)
+  unsigned int m_velocity;                    //Скорость (для движущихся объектов)
+
+public:
+  explicit IMapItem( maplib::ItemType type );
+  explicit IMapItem( maplib::ItemType type, maplib::DirectionType dirType, unsigned int Health, unsigned int Velocity );
+  explicit IMapItem( const IMapItem& other );
+  virtual ~IMapItem();
+
+  IMapItem& operator=( const IMapItem& other );
+
+  void setType( maplib::ItemType type );
+  maplib::ItemType getType()const;
+
+  void setHealth( unsigned int health );
+  unsigned int getHealth()const;
+
+  void setVelocity( unsigned int velocity );
+  unsigned int getVelocity()const;
+
+  void setDirection( maplib::DirectionType directionType );
+  maplib::DirectionType getDirection()const;
+};
+
+}//namespace gelib
+
 //=========================================================================
 
 #endif //__IMAPITEM_H__
---
+++ gelib/0.2/src/imapitem.cpp
@@ -0,0 +1,104 @@
+/*                imapitem.cpp
+      created by infinitydao@gmail.com
+                  09/2013
+          General Publing License v 2.0
+*/
+
+#include "imapitem.h"
+
+//-------------------------------------------------------------------------
+gelib::IMapItem::IMapItem( maplib::ItemType type )
+:m_type( type )
+{
+
+}
+
+//-------------------------------------------------------------------------
+gelib::IMapItem::IMapItem( maplib::ItemType type, maplib::DirectionType dirType, unsigned int Health, unsigned int Velocity )
+:m_type(type),
+m_directionType(dirType),
+m_health(Health),
+m_velocity(Velocity)
+{
+
+}
+
+//-------------------------------------------------------------------------
+gelib::IMapItem::IMapItem( const IMapItem& other )
+:m_type(other.m_type),
+m_directionType(other.m_directionType),
+m_health(other.m_health),
+m_velocity(other.m_velocity)
+{
+  
+}
+
+//-------------------------------------------------------------------------
+gelib::IMapItem::~IMapItem()
+{
+
+}
+
+//-------------------------------------------------------------------------
+gelib::IMapItem& gelib::IMapItem::operator=( const IMapItem& other )
+{
+  if( &other == this )
+    return *this;
+
+  m_type = other.m_type;
+  m_directionType = other.m_directionType;
+  m_health = other.m_health;
+  m_velocity = other.m_velocity;
+
+  return *this;
+}
+
+//-------------------------------------------------------------------------
+void gelib::IMapItem::setType( maplib::ItemType type )
+{
+  m_type = type;
+}
+
+//-------------------------------------------------------------------------
+maplib::ItemType gelib::IMapItem::getType()const
+{
+  return m_type;
+}
+
+//-------------------------------------------------------------------------
+void gelib::IMapItem::setHealth( unsigned int health )
+{
+  m_health = health;
+}
+
+//-------------------------------------------------------------------------
+unsigned int gelib::IMapItem::getHealth()const
+{
+  return m_health;
+}
+
+//-------------------------------------------------------------------------
+void gelib::IMapItem::setVelocity( unsigned int velocity )
+{
+  m_velocity = velocity;
+}
+
+//-------------------------------------------------------------------------
+unsigned int gelib::IMapItem::getVelocity()const
+{
+  return m_velocity;
+}
+
+//-------------------------------------------------------------------------
+void gelib::IMapItem::setDirection( maplib::DirectionType directionType )
+{
+  m_directionType = directionType;
+}
+
+//-------------------------------------------------------------------------
+maplib::DirectionType gelib::IMapItem::getDirection()const
+{
+  return m_directionType;
+}
+
+//-------------------------------------------------------------------------
