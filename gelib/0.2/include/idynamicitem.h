2653124690e34cbbfb4db6724924bb7ba6fbb45a
1 <1>
2013-09-18 15:23:52 +0400

    Added idynamicitem.h

--- gelib/0.2/include/idynamicitem.h
+++ gelib/0.2/include/idynamicitem.h
@@ -1,18 +1,18 @@
-/*                gelib.h
+/*              idynamicitem.h
       created by infinitydao@gmail.com
                   09/2013
           General Publing License v 2.0
 */
 
-#ifndef __GELIB_H__
-#define __GELIB_H__
-
-#include "imapitem.h"
-#include "idrawitem.h"
-#include "istaticitem.h"
-#include "idynamicitem.h"
+#ifndef __IDYNAMICITEM_H__
+#define __IDYNAMICITEM_H__
 
 //=========================================================================
+namespace gelib
+{
+
+} //namespace gelib
+
 //=========================================================================
 
-#endif //__GELIB_H__
+#endif //__IDYNAMICITEM_H__
